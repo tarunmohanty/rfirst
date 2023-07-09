43a159017a5dc0803801aa6761c5b358ea51a0ce
tarunmohanty <tarunmohantys@gmail.com>
2023-07-09 20:39:21 +0530

    Update hurricanes.R

--- hurricanes.R
+++ hurricanes.R
@@ -1,1 +1,3 @@
-data <- read.csv('https://website.com/data.csv')
+url <- read.csv("https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2017-financial-year-provisional/Download-data/annual-enterprise-survey-2017-financial-year-provisional-csv.csv",header=TRUE)
+
+ 
