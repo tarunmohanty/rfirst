ba0f98165c65fcf3572ff154f6083529c08bce94
tarunmohanty <tarunmohantys@gmail.com>
2023-07-09 20:23:23 +0530

    Update hurricanes.R

--- hurricanes.R
+++ hurricanes.R
@@ -1,1 +1,1 @@
-employees <- read.csv("http://www.headfirstlabs.com/books/hfda/hfda_ch09_employees.csv",header=TRUE)
+employees <- read.csv("http://www.headfirstlabs.com/books/hfda/hfda_ch09_employees.csv")
