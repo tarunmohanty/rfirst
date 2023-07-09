5c31b41a48032dee8056109556b4bd09836fe917
tarunmohanty <tarunmohantys@gmail.com>
2023-07-09 20:19:40 +0530

    Update hurricanes.R

--- hurricanes.R
+++ hurricanes.R
@@ -1,1 +1,2 @@
-hurricanes <- read.csv("https://people.sc.fsu.edu/~jburkardt/data/csv/letter_frequency.csv", header=TRUE)
+hurricanes <- read.csv("http://www.people.sc.fsu.edu/~jburkardt/data/csv/letter_frequency.csv",header=TRUE)
+
